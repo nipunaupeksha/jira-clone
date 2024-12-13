e76fcd5721e3217aed887acebec9037b81c25f80
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-13 10:23:28 +0200

    add: sign-in with Hono.js fixed

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -16,9 +16,13 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
+
 import { loginSchema } from "../schemas";
+import { useLogin } from "../api/use-login";
 
 export const SignInCard = () => {
+  const { mutate } = useLogin();
+
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -28,7 +32,7 @@ export const SignInCard = () => {
   });
 
   const onSubmit = (values: z.infer<typeof loginSchema>) => {
-    console.log({ values });
+    mutate({ json: values });
   };
 
   return (
