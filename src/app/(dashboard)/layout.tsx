da87ad1c315642671e11b8e31612c5468788c508
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-19 00:34:55 +0200

    add: sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,11 +10,11 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
     <div className="min-h-screen">
       <div className="flex w-full h-full">
         <div className="fixed lext-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto">
-          <Sidebar/>
+          <Sidebar />
         </div>
-        <div className="lg:pl-[264px]">
+        <div className="lg:pl-[264px] w-full">
           <div className="mx-auto max-w-screen-2xl h-full">
-            <Navbar/>
+            <Navbar />
             <main className="h-full py-8 px-6 flex flex-col">{children}</main>
           </div>
         </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,6 @@
+import { UserButton } from "@/features/auth/components/user-button";
+import { MobileSidebar } from "./mobile-sidebar";
+
 export const Navbar = () => {
   return (
     <nav className="pt-4 px-6 flex items-center justify-between">
@@ -5,6 +8,8 @@ export const Navbar = () => {
         <h1 className="text-2xl font-semibold">Home</h1>
         <p className="text-muted-foreground">Monitor all of your projects and tasks here</p>
       </div>
+      <MobileSidebar/>
+      <UserButton/>
     </nav>
   );
 };
